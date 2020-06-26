cfca04bc296db527060dc3a846d88de21d782608
Raul Gonzalez <bragr77@gmail.com>
2020-06-26 10:38:30 -0400

    agregando css a nuestro componente

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ container.appendChild(element); */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const container = document.getElementById('root')
 //ReactDOM.render (_QUE_ , _DONDE_)
